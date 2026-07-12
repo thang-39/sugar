130c7893cda829890c9a841280dcdd8af348055d
Minh Thang <trantruongminhthang@gmail.com>
2026-07-12 09:50:31 +0700

    fix bug about back button

--- app/(tabs)/settings/_layout.tsx
+++ app/(tabs)/settings/_layout.tsx
@@ -17,11 +17,6 @@ export default function SettingsStackLayout(): ReactElement {
       }}
     >
       <Stack.Screen name="index" options={{ title: t('tabs.settings') }} />
-      <Stack.Screen name="target-range" options={{ title: t('screens.settings.targetRange.title') }} />
-      <Stack.Screen name="tracking-mode" options={{ title: t('screens.settings.trackingMode.title') }} />
-      <Stack.Screen name="reminders" options={{ title: t('reminders.title') }} />
-      <Stack.Screen name="report" options={{ title: t('screens.settings.report.title') }} />
-      <Stack.Screen name="about" options={{ title: t('screens.settings.about.title') }} />
     </Stack>
   );
 }
